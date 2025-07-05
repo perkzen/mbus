828a3b34878d8a7fb811b86be8533cfab8a382cc
perkzen <perko.domen@gmail.com>
2025-07-05 14:46:16 +0200

    chore: add ESLint and Prettier configuration files

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { Link } from '@tanstack/react-router'
+import { Link } from "@tanstack/react-router";
 
 export default function Header() {
   return (
@@ -9,5 +9,5 @@ export default function Header() {
         </div>
       </nav>
     </header>
-  )
+  );
 }
--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,13 +1,9 @@
-import {createFileRoute} from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/')({
-    component: App,
-})
+export const Route = createFileRoute("/")({
+  component: App,
+});
 
 function App() {
-    return (
-        <div>
-            test
-        </div>
-    )
+  return <div>test</div>;
 }
